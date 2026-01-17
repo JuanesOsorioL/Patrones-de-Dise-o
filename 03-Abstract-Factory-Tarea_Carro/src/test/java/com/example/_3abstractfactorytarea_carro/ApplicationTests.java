32f50d7a9a5748199c750eb00ca5af4733f42e2b
JuanesOsorioL <juanchis_1989@hotmail.com>
2026-01-16 20:46:54 -0500

    se implementa el patron Abstract Factory -> se realiza la tarea del Abstract Factory

---
+++ 03-Abstract-Factory-Tarea_Carro/src/test/java/com/example/_3abstractfactorytarea_carro/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._3abstractfactorytarea_carro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
