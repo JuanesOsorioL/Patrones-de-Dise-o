9bff52adfa6fdebd404d7b51c0ffa5716d53b1da
JuanesOsorioL <juanchis_1989@hotmail.com>
2026-01-21 14:44:02 -0500

    se implementa otro ejemplo llamado pokemon, del patron prototype

--- patrones-creacionales/04-Prototype-Tarea_Pokemon/src/test/java/com/example/_4prototypetarea_pokemon/ApplicationTests.java
+++ patrones-creacionales/04-Prototype-Tarea_Pokemon/src/test/java/com/example/_4prototypetarea_pokemon/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example._3abstractfactorytarea_carro;
+package com.example._4prototypetarea_pokemon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
