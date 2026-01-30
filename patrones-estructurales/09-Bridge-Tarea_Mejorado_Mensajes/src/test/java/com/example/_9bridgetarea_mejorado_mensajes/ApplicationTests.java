dd790e2688e13da6cf9f4379f4acc25629b7f149
JuanesOsorioL <juanchis_1989@hotmail.com>
2026-01-30 18:10:54 -0500

    patron bridge -> se inicia una mejora de la Tarea de tipos de mensajeria

---
+++ patrones-estructurales/09-Bridge-Tarea_Mejorado_Mensajes/src/test/java/com/example/_9bridgetarea_mejorado_mensajes/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._9bridgetarea_mejorado_mensajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
